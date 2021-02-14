2cad2edded45804b5e393d98b255fd20236c6360
lorenzo99-droid <lorenzoferr.mar@gmail.com>
2021-02-13 22:37:00 -0300

    Pasta myAPI deletada

--- myapi/src/controllers/tarefas-controller.js
+++
@@ -1,7 +0,0 @@
-app.get(`/tarefas`, (req, res) => {
-    res.send(`Rota ativada com GET e recurso tarefa: valores de tarefa devem ser retornados`)
-})
-
-app.post(`/tarefas`, (req, res) => {
-    res.send(`Rota ativada com POST e recurso tarefa: valores de tarefa devem ser retornados`)
-})
--- myapi/src/controllers/usuarios-controller.js
+++
@@ -1,7 +0,0 @@
-app.get(`/usuarios`, (req, res) => {
-    res.send(`Rota ativada com GET e recurso usuario: valores de usuario devem ser retornados`)
-})
-
-app.post(`/usuarios`, (req, res) => {
-    res.send(`Rota ativada com POST e recurso usuario: valores de usuario devem ser retornados`)
-})
