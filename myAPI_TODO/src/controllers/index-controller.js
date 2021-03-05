9028198c56e438d1423c6d4a1a02dd6c657cea6d
lorenzo99-droid <lorenzoferr.mar@gmail.com>
2021-03-05 16:23:40 -0300

    Arquivo my_API_TODO excluído

--- myAPI_TODO/src/controllers/index-controller.js
+++
@@ -1,8 +0,0 @@
-module.exports = (app) =>
-    {
-        app.get('/', 
-            (req, res) =>
-                {res.send('Esse é o index')}
-        );
-    };
-    
--- myAPI_TODO/src/models/usuarios-model.js
+++
@@ -1,7 +0,0 @@
-module.exports = class Usuario {
-    constructor (nome,email,senha){
-        this.nome = nome;
-        this.email = email;
-        this.senha = senha;
-    }
-}
